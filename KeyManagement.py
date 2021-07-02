0a25362c3f251368820e5a282e2fea40fde3d32d
GREGFOO\User <gregoryfoo@hotmail.com>
2021-07-02 19:29:48 +0800

    Copied from guide

--- KeyManagement.py
+++ KeyManagement.py
@@ -16,7 +16,17 @@ import gspread
 from google.oauth2.service_account import Credentials
 from gspread_pandas import Spread, Client
 
+from google.oauth2 import service_account
+from gsheetsdb import connect
 
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
 
 #---------------------------------#
 # Page layout
@@ -41,3 +51,17 @@ st.sidebar.header('User Input Features')
 #
 #@st.cache(persist=True,show_spinner = True)
 #def load_data():
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
