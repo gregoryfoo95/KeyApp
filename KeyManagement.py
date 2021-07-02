e62d7e0f7da6f1dbc816e7fc0f1b2e2f58673f2d
Gregory Foo <57233318+gregoryfoo95@users.noreply.github.com>
2021-07-02 16:44:50 +0800

    Add files via upload

--- KeyManagement.py
+++ KeyManagement.py
@@ -34,21 +34,4 @@ st.sidebar.header('User Input Features')
 # Web scraping of data.gov.sg data
 #
 @st.cache(persist=True,show_spinner = True)
-def load_data():
-    myDataFrame = pd.DataFrame()
-#    urldata = ['https://storage.data.gov.sg/resale-flat-prices/resources/resale-flat-prices-based-on-registration-date-from-jan-2017-onwards-2021-06-04T02-52-32Z.csv']
-    #, 'https://storage.data.gov.sg/resale-flat-prices/resources/resale-flat-prices-based-on-registration-date-from-jan-2015-to-dec-2016-2019-06-17T09-03-16Z.csv', \
-    #       'https://storage.data.gov.sg/resale-flat-prices/resources/resale-flat-prices-based-on-registration-date-from-mar-2012-to-dec-2014-2019-06-17T09-04-34Z.csv', \
-    #       'https://storage.data.gov.sg/resale-flat-prices/resources/resale-flat-prices-based-on-approval-date-2000-feb-2012-2019-06-28T10-14-13Z.csv', \
-    #       'https://storage.data.gov.sg/resale-flat-prices/resources/resale-flat-prices-based-on-approval-date-1990-1999-2021-05-25T02-49-29Z.csv']
-#    for links in urldata:
-#        req = Request(links)
-#        req.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0')
-#        content = urlopen(req)
-    df = pd.read_csv('resale-flat-prices-based-on-registration-date-from-jan-2017-onwards.csv')
-    myDataFrame = myDataFrame.append(df)
-    myDataFrame.sort_values(by = ['month'],ascending = False)
-    return myDataFrame
-
-myDataFrame = load_data()
-town = myDataFrame.groupby(['town','flat_type'])
+#def load_data():
