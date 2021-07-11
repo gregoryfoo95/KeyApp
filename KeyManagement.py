ba7925353dc2ced0565816df277e756b02fec41c
GREGFOO\User <gregoryfoo@hotmail.com>
2021-07-12 01:51:50 +0800

    no message

--- KeyManagement.py
+++ KeyManagement.py
@@ -17,6 +17,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 from gsheetsdb import connect
 from gspread_pandas import Spread,Client
 import gspread as gc
+from df2gspread import df2gspread as d2g
 #---------------------------------#
 # Page layout
 ## Page expands to full width
@@ -33,8 +34,25 @@ st.title('Key Management App')
 st.markdown("""
 **AWOF 805 Key Status.**
 """)
-username = st.text_input("Please key in your name")
-st.sidebar.header('Choose Key Numbers which you wish to draw.')
+
+st.sidebar.header('Select the Keys and Location it is drawn to.')
+
+#Creating User Input UI for key selection
+#key_list = sorted(df_temp['S/N'].unique())
+
+with st.form("Details"):
+    username = st.text_input("Please key in your name")
+    st.form_submit_button(label='Submit',help='Press to confirm details')
+
+if username == None:
+    print("Missing username.")
+else:
+    key_list = list(range(1,68))
+    selected_key = st.sidebar.multiselect('S/N', key_list)
+    selected_keylist = list(selected_key)
+    loc_list = ['Keypress','FMC','EGR Bay','Gun Bay','WO Office','MS Office','Ops Office','Project Room',' OIC Office', 'OC Office']
+    selected_loc = st.sidebar.multiselect('Location',loc_list)
+
 
 
 # Create a connection object.
@@ -49,6 +67,18 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('key-management-3
 client = gc.authorize(credentials)
 spreadsheets = ['Key Management']
 
+@st.cache()
+def display(spreadsheets):
+    for spreadsheet in spreadsheets:
+        sh=client.open(spreadsheet)
+        worksheet = sh.get_worksheet(0)
+        data = worksheet.get_all_values()
+        df_temp = pd.DataFrame(columns = [i for i in range(len(data[0]))])
+        df_temp.columns = ['S/N','Name','Location']
+        for i in range(1,len(data)):
+            df_temp.loc[len(df_temp)] = data[i]
+
+    return df_temp
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache()
@@ -57,29 +87,29 @@ def main(spreadsheets):
     for spreadsheet in spreadsheets:
 		#Open the Spreadsheet
         sh = client.open(spreadsheet)
-
 		#Get all values in the first worksheet
         worksheet = sh.get_worksheet(0)
+        #Updating Google Sheet
+        for keynum in selected_keylist:
+            worksheet.update_cell(keynum+1,2,username)
+            if not selected_loc:
+                break
+            else :
+                worksheet.update_cell(keynum+1,3,selected_loc[0])
+
         data = worksheet.get_all_values()
         #Save the data inside the temporary pandas dataframe
         df_temp = pd.DataFrame(columns = [i for i in range(len(data[0]))])
-        df_temp.columns = ['S/N', 'Name', 'Location']
+        df_temp.columns = ['S/N','Name','Location']
         for i in range(1,len(data)):
             df_temp.loc[len(df_temp)] = data[i]
 
     return df_temp
     #return df_temp
 
-#Printing of Key Status
-df_temp = main(spreadsheets)
+#Printing/Updating of Key Status
+if username != '':
+    df_temp = main(spreadsheets)
+else :
+    df_temp = display(spreadsheets)
 st.table(df_temp)
-
-#Creating User Input UI for key selection
-key_list = sorted(df_temp['S/N'].unique())
-selected_key = st.sidebar.multiselect('S/N', key_list)
-loc_list = ['Keypress','FMC','EGR Bay','Gun Bay','WO Office','MS Office','Ops Office','Project Room',' OIC Office', 'OC Office']
-selected_loc = st.sidebar.multiselect('Location',loc_list)
-
-# Creating another connection to google sheet
-sheet = client.open(spreadsheets[0])
-sheet.update_cell(1,1,"hello")
